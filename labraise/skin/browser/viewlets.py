6d6295c00421f9e2344df01a18a33fdadd2bd96d
Alain Meurant <alain.meurant@affinitic.be>
2009-05-29 09:19:01 +0000

    mise en place header, bandeau, logo, personnalbar, selectorlangue, breadcrumb, footer; resizing du portal (encore à ajuster); centrage du portal

---
+++ labraise/skin/browser/viewlets.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+labraise.skin
+
+Licensed under the GPL license, see LICENCE.txt for more details.
+Copyright by Affinitic sprl
+
+$Id: viewlets.py 4587 2009-02-22 22:32:37Z alain
+"""
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets.common import GlobalSectionsViewlet
+from zope.component import getMultiAdapter
+
+class LaBraiseSectionsViewlet(GlobalSectionsViewlet):
+    render = ViewPageTemplateFile('templates/header_labraise.pt')
+
+    def logo_tag(self):
+        portal_state = getMultiAdapter((self.context, self.request),
+                                        name=u'plone_portal_state')
+        portal = portal_state.portal()
+        logoName = portal.restrictedTraverse('base_properties').logoName
+        return portal.restrictedTraverse(logoName).tag()
+
+class LaBraiseFooterViewlet(GlobalSectionsViewlet):
+    render = ViewPageTemplateFile('templates/footer_labraise.pt')
